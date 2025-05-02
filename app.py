6673c5f8c376c8cef2cf0655d6070c25229eb6fe
Ahmed Talima <162712731+AhmedTalima23@users.noreply.github.com>
2025-05-02 19:02:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -177,18 +177,9 @@ def render_header():
         st.markdown("""
         <div class="header-text">
             <h1 class="header-title">👩‍💼Employee Churn Prediction</h1>
-            <div style="margin-top: 20px; background-color: rgba(255,255,255,0.2); padding: 12px; border-radius: 8px;">
-                <p style="margin: 5px 0; font-size: 1rem; font-weight: 500;">Developed by:</p>
-                <div style="display: flex; flex-wrap: wrap; gap: 8px 15px; margin: 8px 0;">
-                    <span>Ahmed Mohamed</span>
-                    <span>• Theodore Naguib</span>
-                    <span>• Malak Torky</span>
-                    <span>• Shrouk Emam</span>
-                    <span>• Salah Eldin Mohamed</span>
-                    <span>• Seif Ahmed</span>
-                </div>
-                <p style="margin: 5px 0; font-size: 1rem;">Supervised by: <span style="font-weight: 500;">Eng. Mahmoud Talaat</span></p>
-            </div>
+            <p class="header-subtitle">
+                Predict churn risks • Improve retention • Strengthen your workforce
+            </p>
         </div>
         """, unsafe_allow_html=True)
 
